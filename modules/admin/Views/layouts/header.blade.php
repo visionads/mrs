9240f9f07a77c5aa9bccf7e27fe9c8d0a1a7904a
Ram Paul <ram@edutechsolutionsbd.com>
2016-05-22 10:29:54 +0600

    header page and navigation style

--- modules/admin/Views/layouts/header.blade.php
+++ modules/admin/Views/layouts/header.blade.php
@@ -101,7 +101,7 @@
                                 </a>
                                 <ul class="dropdown-menu">
                                     <li><a href="{{route('user-profile')}}">Profile</a></li>
-                                    <li><a href="#">User Settings</a></li>
+                                    {{--<li><a href="#">User Settings</a></li>--}}
                                     <li><a href="{{route('settings')}}">Settings</a></li>
                                     <li role="separator" class="divider"></li>
                                     <li><a href="{{route('user-logout')}}">Log Out</a></li>
