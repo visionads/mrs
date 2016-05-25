467cbc68c3fe349efd98fbaf8c72bda58b871e07
Almas Hossain <almas@edutechsolutionsbd.com>
2016-05-25 16:11:59 +0600

    user

--- modules/user/database/sql_dump/user.sql
+++ modules/user/database/sql_dump/user.sql
@@ -3,7 +3,7 @@
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `email`, `password`, `auth_key`, `access_token`, `csrf_token`, `ip_address`, `branch_id`, `last_visit`, `expire_date`, `remember_token`, `status`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `auth_key`, `access_token`, `csrf_token`, `ip_address`, `business_id`, `last_visit`, `expire_date`, `remember_token`, `status`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
 (1, 'super-admin', 'devdhaka407@gmail.com', '$2y$10$fh/TeCj9g46sJj7yvSK9ReHrdJcM57zIzNg0yR/CExV/sd6M1TCPi', '', '', '', '127.0.1.1',1,'2015-08-27 16:33:22','2020-08-27 16:33:22', '', 'active', 0, 0, '2016-02-23 02:57:55', '2016-02-23 02:57:55'),
 (2, 'system-admin', 'devdhaka409@gmail.com', '$2y$10$fh/TeCj9g46sJj7yvSK9ReHrdJcM57zIzNg0yR/CExV/sd6M1TCPi', '', '', '', '127.0..1',1, '2015-08-27 16:33:22','2020-08-27 16:33:22', '', 'active', 0, 0, '2016-02-23 02:57:55', '2016-02-23 02:57:55'),
 (3, 'admin', 'admin@admin.com', '$2y$10$fh/TeCj9g46sJj7yvSK9ReHrdJcM57zIzNg0yR/CExV/sd6M1TCPi', '', '', '', '127.0.1.1', 1,'2015-08-27 16:33:22','2020-08-27 16:33:22', '', 'active', 0, 0, '2016-02-23 02:57:55', '2016-02-23 02:57:55'),
