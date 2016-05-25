88f411678ae59f25433d080a88d3afbe72de4e03
Almas Hossain <almas@edutechsolutionsbd.com>
2016-05-25 16:19:21 +0600

    user

--- modules/user/database/sql_dump/user.sql
+++ modules/user/database/sql_dump/user.sql
@@ -4,10 +4,10 @@
 --
 
 INSERT INTO `user` (`id`, `username`, `email`, `password`, `auth_key`, `access_token`, `csrf_token`, `ip_address`, `business_id`, `last_visit`, `expire_date`, `remember_token`, `status`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
-(1, 'super-admin', 'devdhaka407@gmail.com', '$2y$10$fh/TeCj9g46sJj7yvSK9ReHrdJcM57zIzNg0yR/CExV/sd6M1TCPi', '', '', '', '127.0.1.1',1,'2015-08-27 16:33:22','2020-08-27 16:33:22', '', 'active', 0, 0, '2016-02-23 02:57:55', '2016-02-23 02:57:55'),
-(2, 'system-admin', 'devdhaka409@gmail.com', '$2y$10$fh/TeCj9g46sJj7yvSK9ReHrdJcM57zIzNg0yR/CExV/sd6M1TCPi', '', '', '', '127.0..1',1, '2015-08-27 16:33:22','2020-08-27 16:33:22', '', 'active', 0, 0, '2016-02-23 02:57:55', '2016-02-23 02:57:55'),
-(3, 'admin', 'admin@admin.com', '$2y$10$fh/TeCj9g46sJj7yvSK9ReHrdJcM57zIzNg0yR/CExV/sd6M1TCPi', '', '', '', '127.0.1.1', 1,'2015-08-27 16:33:22','2020-08-27 16:33:22', '', 'active', 0, 0, '2016-02-23 02:57:55', '2016-02-23 02:57:55'),
-(4, 'accountant', 'devdhaka408@gmail.com', '$2y$10$0Mskcpfs8t6zSyMILPYOzenk1rs61P.kv0pNs/zIDVdPSU1F4vAw2', '', '', '', '127.0.1.1',1, '2015-08-27 16:33:22','2020-08-27 16:33:22', '', 'active', 0, 0, '2016-02-23 02:57:55', '2016-02-23 02:57:55'),
-(5, 'junior-accountant', 'devdhaka403@gmail.com', '$2y$10$0Mskcpfs8t6zSyMILPYOzenk1rs61P.kv0pNs/zIDVdPSU1F4vAw2', '', '', '','127.0.1.1', 1, '2015-08-27 16:33:22','2020-08-27 16:33:22', '', 'active', 0, 0, '2016-02-23 02:57:55', '2016-02-23 02:57:55');
+(1, 'super-admin', 'devdhaka407@gmail.com', '$2y$10$fh/TeCj9g46sJj7yvSK9ReHrdJcM57zIzNg0yR/CExV/sd6M1TCPi', '', '', '', '127.0.1.1','','2015-08-27 16:33:22','2020-08-27 16:33:22', '', 'active', 0, 0, '2016-02-23 02:57:55', '2016-02-23 02:57:55'),
+(2, 'system-admin', 'devdhaka409@gmail.com', '$2y$10$fh/TeCj9g46sJj7yvSK9ReHrdJcM57zIzNg0yR/CExV/sd6M1TCPi', '', '', '', '127.0..1','', '2015-08-27 16:33:22','2020-08-27 16:33:22', '', 'active', 0, 0, '2016-02-23 02:57:55', '2016-02-23 02:57:55'),
+(3, 'admin', 'admin@admin.com', '$2y$10$fh/TeCj9g46sJj7yvSK9ReHrdJcM57zIzNg0yR/CExV/sd6M1TCPi', '', '', '', '127.0.1.1', '','2015-08-27 16:33:22','2020-08-27 16:33:22', '', 'active', 0, 0, '2016-02-23 02:57:55', '2016-02-23 02:57:55'),
+(4, 'accountant', 'devdhaka408@gmail.com', '$2y$10$0Mskcpfs8t6zSyMILPYOzenk1rs61P.kv0pNs/zIDVdPSU1F4vAw2', '', '', '', '127.0.1.1','', '2015-08-27 16:33:22','2020-08-27 16:33:22', '', 'active', 0, 0, '2016-02-23 02:57:55', '2016-02-23 02:57:55'),
+(5, 'junior-accountant', 'devdhaka403@gmail.com', '$2y$10$0Mskcpfs8t6zSyMILPYOzenk1rs61P.kv0pNs/zIDVdPSU1F4vAw2', '', '', '','127.0.1.1', '', '2015-08-27 16:33:22','2020-08-27 16:33:22', '', 'active', 0, 0, '2016-02-23 02:57:55', '2016-02-23 02:57:55');
 
 
