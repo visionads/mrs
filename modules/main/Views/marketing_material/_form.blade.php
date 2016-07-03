988f923f6fb9799c882500be6b3f3ebbf17ac644
Ram Paul <ram@edutechsolutionsbd.com>
2016-07-03 17:28:16 +0600

    up

--- modules/main/Views/marketing_material/_form.blade.php
+++ modules/main/Views/marketing_material/_form.blade.php
@@ -18,53 +18,42 @@
 </div>--}}
 <div class="row">
     <div class="col-md-6">
-    <label class="green-yellow">Quantity : </label>
-    <select class="deeppink select-inpt size-15" id="qty" onchange="totalCalculation()">
-        <option>01</option>
-        <option>02</option>
-        <option>03</option>
-        <option>04</option>
-        <option>05</option>
-        <option>06</option>
-        <option>07</option>
-    </select>
-    <label class="green-yellow">Stock : </label>
-    <select class="deeppink select-inpt size-15" id="stock" onchange="totalCalculation()">
-        <option>01</option>
-        <option>02</option>
-        <option>03</option>
-        <option>04</option>
-        <option>05</option>
-        <option>06</option>
-        <option>07</option>
-    </select>
+        <label class="green-yellow">Quantity : </label>
+        <select class="deeppink select-inpt size-15" id="qty" onchange="myFunction()">
+            <option>0</option>
+            <option>01</option>
+            <option>02</option>
+            <option>03</option>
+            <option>04</option>
+            <option>05</option>
+            <option>06</option>
+            <option>07</option>
+        </select>
 
+        <label class="green-yellow">Stock : </label>
+        <select class="deeppink select-inpt size-15" id="stock" onchange="myFunction()" >
+            <option>0</option>
+            <option>01</option>
+            <option>02</option>
+            <option>03</option>
+            <option>04</option>
+            <option>05</option>
+            <option>06</option>
+            <option>07</option>
+        </select>
     </div>
 </div>
 <div class="row">
     <div class="col-md-6">
-        <h3 class="green-yellow">Total : $100</h3>
+        <h3 class="green-yellow" id="total">Total : $100</h3>
     </div>
 </div>
-
 <script>
-
-    function totalCalculation() {
-        if(document.getElementById(qty).value){
-            var q = document.getElementById(qty).value;
-        }else{
-            var q = 0;
-        }
-
-        if(document.getElementById(stock).value){
-            var s = document.getElementById(stock).value;
-        }else{
-            var s = 0;
-        }
-
-        var total = 0;
-        total = q + s;
-        alert(total);
-        document.getElementById("demo").innerHTML = "You selected: " + x;
+    function myFunction() {
+        var x = document.getElementById("qty").value;
+        var y = document.getElementById("stock").value;
+        var ttl = 0;
+        ttl = parseInt(x+y);
+        document.getElementById("total").innerHTML = "Total : $" + ttl;
     }
 </script>
