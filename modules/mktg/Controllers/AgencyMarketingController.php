9777e235cacb76882381d987fa45661497add47c
shajjadhossain <shajjadhossain81@gmail.com>
2016-07-27 11:57:42 +0600

    mrs

--- modules/mktg/Controllers/AgencyMarketingController.php
+++ modules/mktg/Controllers/AgencyMarketingController.php
@@ -36,15 +36,14 @@ class AgencyMarketingController extends Controller
         $pageTitle = $title['title'];
         $data = MktgMenuItem::with('relMktgMenuItemImage','relMktgItemOption','relMktgItemOption.relMktgItemValue')->where('slug',$slug)->get()->toArray();
 
-
-
-
-        #$data['value'] = DB::table('mktg_item_value')->lists('title', 'id');
+        $value = DB::table('mktg_item_value')->lists('title', 'id');
+        #print_r($value[3]);exit;
         /*return view('mktg::marketing_material.agency_stationary_materials.index',$data,$value);*/
 
         return view('mktg::marketing_material.agency_stationary_materials.index',array(
             'pageTitle'=>$pageTitle,
             'data'=>$data,
+            'value'=>$value,
         ));
 
     }
