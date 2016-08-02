5a0bba25a2d04c801a96876a70bd7ee2fa181e5e
Ram Paul <ram@edutechsolutionsbd.com>
2016-08-02 10:36:06 +0600

    Admin Settings dashboard and custom.css have been changed

--- modules/admin/Views/settings/dashboard.blade.php
+++ modules/admin/Views/settings/dashboard.blade.php
@@ -65,39 +65,93 @@
 
                         <div class="panel-body no-padding-t text-center" id="settings_button">
 
+                            <div class="form-group">
+                                <a class="btn" href="{{route('local-media')}}">
+                                    <strong>Local Media and Options</strong>
+                                </a>
+                            </div>
+
+                            <br>
+
+                            <div class="form-group">
+                                <a class="btn" href="{{ route('quote-invoice-settings') }}">
+                                    <strong>Quote Number / Order Number</strong>
+                                </a>
+                            </div>
+
+                            <br>
+
+                            <div class="form-group">
+                                <a class="btn" href="{{ route('quote-invoice-settings') }}">
+                                    <strong>Invoice Number</strong>
+                                </a>
+                            </div>
+
+                            <br>
+
+                            <div class="form-group">
+                                <a class="btn" href="{{ route('quote-invoice-settings') }}">
+                                    <strong>Transaction Number</strong>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <hr>
+
+
+        {{--Settings Part-2 Start Here--}}
+        <div class="row" id="button_heading">
+
+        <div class="col-sm-12" id="setting_bottons"><span class="label green-yellow">Settings(Part-2)</span></div>
+
+        <div class="col-sm-6">
+            <div class="panel1" id="button_align">
+                <div id="report-button">
+
+                    <div class="panel-body no-padding-t text-center" id="settings_button_one">
                         <div class="form-group">
-                            <a class="btn" href="{{route('local-media')}}">
-                                <strong>Local Media and Options</strong>
+                            <a class="btn" href="{{route('mktg-menu-item')}}">
+                                <strong>Marketing Material Menu Item</strong>
                             </a>
                         </div>
 
-                        <br>
-
-                        <div class="form-group">
-                            <a class="btn" href="{{ route('quote-invoice-settings') }}">
-                                <strong>Quote Number / Order Number</strong>
+                        {{--<div class="form-group">
+                            <a class="btn" href="{{route('photography-package')}}">
+                                <strong>Menu Name</strong>
                             </a>
-                        </div>
-
-                        <br>
-
-                        <div class="form-group">
-                            <a class="btn" href="{{ route('quote-invoice-settings') }}">
-                                <strong>Invoice Number</strong>
-                            </a>
-                        </div>
-
-                        <br>
-
-                        <div class="form-group">
-                            <a class="btn" href="{{ route('quote-invoice-settings') }}">
-                                <strong>Transaction Number</strong>
-                            </a>
-                        </div>
+                        </div>--}}
                     </div>
                 </div>
             </div>
         </div>
 
+        <div class="col-sm-6">
+            <div class="panel1" id="button_align">
+                <div id="report-button">
+
+                    <div class="panel-body no-padding-t text-center" id="settings_button_two">
+
+                        {{--<div class="form-group">
+                            <a class="btn" href="{{route('local-media')}}">
+                                <strong>Local Media and Options</strong>
+                            </a>
+                        </div>--}}
+                        {{--<div class="form-group">
+                            <a class="btn" href="{{ route('quote-invoice-settings') }}">
+                                <strong>Navigation button</strong>
+                            </a>
+                        </div>--}}
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </div>
 @stop
