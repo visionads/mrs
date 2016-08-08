b58546ab95744448cc6c9208276ac5d7882e5385
Selim Reza <me@selimreza.com>
2016-08-08 15:31:42 +0600

    UP

--- modules/user/database/sql_dump/settings.sql
+++ modules/user/database/sql_dump/settings.sql
@@ -3,6 +3,6 @@
 --
 
 INSERT INTO `setting` (`id`, `type`, `code`, `last_number`, `increment`, `status`, `business_id`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
-(1, 'quote-number', 'QN', 20, 1, 1, 2, 0, 6, '0000-00-00 00:00:00', '2016-06-26 04:06:13'),
-(3, 'invoice-number', 'IN', 17, 1, 1, 2, 0, 6, '0000-00-00 00:00:00', '2016-06-23 04:36:06'),
-(4, 'money-receipt-number', 'MR', 3, 1, 1, 2, 0, 6, '0000-00-00 00:00:00', '2016-06-22 23:24:46');
+(1, 'quote-number', 'QN', 20, 1, 1, NULL, 0, 6, '0000-00-00 00:00:00', '2016-06-26 04:06:13'),
+(3, 'invoice-number', 'IN', 17, 1, 1, NULL, 0, 6, '0000-00-00 00:00:00', '2016-06-23 04:36:06'),
+(4, 'money-receipt-number', 'MR', 3, 1, 1, NULL, 0, 6, '0000-00-00 00:00:00', '2016-06-22 23:24:46');
